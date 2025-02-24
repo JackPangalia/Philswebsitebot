b9779741bc6f5fef44f4377eb2ebc871ccd987d7
JackPangalia <jackpangalia1@gmail.com>
2025-02-23 21:18:52 -0800

    worked on refresh bug

--- chatbot/src/components/Message.jsx
+++ chatbot/src/components/Message.jsx
@@ -5,7 +5,7 @@ const Message = ({ messageType, message }) => {
   return (
     <div
       className={`p-4 rounded-3xl text-[14.5px] ${
-        messageType === "ai"
+        messageType === "ai"    
           ? "bg-gray-100 w-9/10"
           : "bg-black text-white w-fit max-w-9/10 ml-auto break-words"
       }`}
